2b156c39526edd78b59f6d078cece1db2f6fe5af
jonah <ju374@nyu.edu>
2020-01-27 01:12:19 -0600

    check input is valid

--- argv/index.js
+++ argv/index.js
@@ -42,7 +42,16 @@ const argv = () => {
         config.ramSize = (config.dimension*config.dimension)*SIZEOF_DOUBLE*3;
     } else if (config.algorithm === "daxpy") {
         config.ramSize = config.dimension*SIZEOF_DOUBLE*3;
+    } else {
+        throw new Error(`Unknown algorithm: "${config.algorithm}"`);
     }
+    if(config.associativity < 1 || !Number.isInteger(args.associativity))
+        throw new Error("Associativity must be more than 0");
+    if(!Object.is(config.blockSize%SIZEOF_DOUBLE, +0))
+        throw new Error("Block size must be a factor of 8");
+    if(!Number.isInteger(config.setsCount))
+        throw new Error("Invalid cache size");
+
     Object.keys(argList).forEach(letter => {
         delete config[letter];
     });
