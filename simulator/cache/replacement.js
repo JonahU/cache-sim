5d0b741406b8926c544b8c5423391042bd6b5704
jonah <ju374@nyu.edu>
2020-01-30 14:54:19 -0600

    replacement fixes

--- simulator/cache/replacement.js
+++ simulator/cache/replacement.js
@@ -13,37 +13,50 @@ var LRUCache = function(capacity) {
     this.LRUTable = {};
     this.capacity = capacity;
     this.nodeCount = 0;
-    this.emptyIndex = null;
 };
 
 LRUCache.prototype.get = function(key) {
-    if (this.LRUTable[key] === undefined) return -1;
-    const toReturn = this.LRUTable[key];
-    this.remove(toReturn);
-    this.add(toReturn);
+    if (this.LRUTable[key] === undefined) throw new Error(`LRU key "${key}" not found in\n${this.LRUTable}`);
+    const willBeRead = this.LRUTable[key];
+    this.remove(willBeRead);
+    this.add(willBeRead);
 };
 
-LRUCache.prototype.put = function(key, value = this.emptyIndex) {
+LRUCache.prototype.put = function(key, overrideValue) {
     const node = this.LRUTable[key];
     if(node) {
         const removed = this.remove(node);
         this.add(node);
-        return removed;
+        return removed.value;
     } else {
         if (this.nodeCount === this.capacity) {
+            // remove LRU node
             const removed = this.remove(this.tail.previous);
-            delete this.LRUTable[removed];
-            this.emptyIndex = removed;
-            return removed;
+            delete this.LRUTable[removed.key];
+
+            // replace with new node
+            const replacementNode = new Node(key, removed.value);
+            this.add(replacementNode);
+            this.LRUTable[key] = replacementNode;
+            return removed.value;
             
         }
-        const newNode = new Node(key, value);
-        const currentCount = this.nodeCount;
+        const newNode = new Node(key, overrideValue);
         this.add(newNode);
         this.LRUTable[key] = newNode;
-        return currentCount;
+        return this.nodeCount;
     }
 };
+
+LRUCache.prototype.getContents = function() {
+    let tempHead = this.head;
+    const recentlyUsedOrder = [];
+    while (tempHead.next && tempHead.next.next) {
+        tempHead = tempHead.next;
+        recentlyUsedOrder.push(tempHead.value);
+    }
+    console.log(`MOST RECENTLY -> ${recentlyUsedOrder.join(" -> ")} -> LEAST RECENTLY`);
+}
 
 LRUCache.prototype.add = function(node) {
     const current_first = this.head.next;
@@ -55,7 +68,7 @@ LRUCache.prototype.add = function(node) {
 }
 
 LRUCache.prototype.remove = function(node) {
-    const removed = node.value;
+    const removed = node;
     const next_node = node.next;
     const prev_node = node.previous;
     next_node.previous = prev_node;
@@ -66,28 +79,15 @@ LRUCache.prototype.remove = function(node) {
 
  class FIFO {
     constructor(capacity) {
-        this.queue = Object.seal(new Array(capacity).fill(null));
+        this.capacity = capacity;
         this.firstIn = 0;
     }
 
-    incrementPtr() {
-        if(this.firstIn === this.queue.length-1) {
-            this.firstIn = -1;
+    getFirstIn() {
+        if(this.firstIn === this.capacity) {
+            this.firstIn = 0;
         }
-        this.firstIn++;
-    }
-
-    put(tag, replaceIndex) {
-        if(replaceIndex) {
-            this.queue[replaceIndex] = tag;
-            this.incrementPtr();
-            return replaceIndex;
-        }
-        const currentFirstIn = this.firstIn;
-        this.queue[currentFirstIn] = tag
-
-        this.incrementPtr();
-        return currentFirstIn;
+        return this.firstIn++;
     }
  }
 
@@ -126,7 +126,7 @@ LRUCache.prototype.remove = function(node) {
             case "LRU":
                 return this.LRU.put(tag, replaceIndex);
             case "FIFO":
-                return this.FIFO.put(tag, replaceIndex)
+                return this.FIFO.getFirstIn();
             case "random":
                 if(replaceIndex) return replaceIndex;
                 return random(this.associativity);
