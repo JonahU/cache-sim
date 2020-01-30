274b93585295bed8f015ef3873b0a8b150e4226c
jonah <ju374@nyu.edu>
2020-01-29 23:12:58 -0600

    time taken

--- simulator/index.js
+++ simulator/index.js
@@ -6,13 +6,27 @@ const simulator = config => {
     let myCpu;
 
     const start = () => {
+        // setup timer
+        const startTime = Date.now();
+
+        // setup & run simulator
         const myRam = new Ram(config);
         const myCache = new Cache(config, myRam);
         myCpu = new Cpu(config, myCache, myRam);
         myCpu.runAlgorithm();
+
+        // stop timer and calculate time taken
+        const endTime = Date.now();
+        const totalTime = new Date(endTime - startTime);
+        const minutes = totalTime.getUTCMinutes();
+        const seconds = totalTime.getUTCSeconds();
+        const timeTaken = minutes > 0 ?
+            `${minutes}m, ${seconds}s` :
+            `${seconds}s`;
         return {
             instructionCount: myCpu.instructionCount,
-            ...myCache.hitsAndMisses()
+            ...myCache.hitsAndMisses(),
+            timeTaken
         }
     };
 
